b28cf5aa9d99e8ba053c40d2772952f644eb6b35
Leandro Ivaciuta <ivaciutaapalache@gmail.com>
2023-11-21 13:06:59 -0300

    todo listo, falta capas unos colores

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+
+/* offset().top es para saber la distancia entre un objeto y la parte superior*/
+
+document.addEventListener('DOMContentLoaded', function () {
+    var navbar = document.querySelector('.div-nav');
+    var altura = navbar.offsetTop;
+
+    window.addEventListener('scroll', function () {
+        if (window.pageYOffset > altura) {
+            navbar.classList.add('div-nav-fixed');
+        } else {
+            navbar.classList.remove('div-nav-fixed');
+        }
+    });
+});
+
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
